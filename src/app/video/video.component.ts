1a0c874a23dd84d99d210fe1a6840bffa5db6c7b
Sergio Garrido <sergarridom8@gmail.com>
2022-11-03 17:10:10 +0100

    cosas iniciales

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -22,31 +22,40 @@ export class VideoComponent implements OnInit, OnDestroy {
       ],
     });
 
-    this.player.on('pause', () => {
-      const contentEl = document.createElement('div');
-      contentEl.innerHTML = `
-      <div>
-        <h1>
-          <strong>Multiple Choice Question</strong>
-        </h1>
-        <br/>
-        <button type="button" class="btn btn-primary" style="background: #007bff">Answer 1</button>
-        <br/><br/>
-        <button type="button" class="btn btn-primary" style="background: #007bff">Answer 2</button>
-        <br/><br/>
-        <button type="button" class="btn btn-primary" style="background: #007bff">Answer 3</button>
-      </div>
-      `;
+    // this.player.on('pause', () => {
+    //   const contentEl = document.createElement('div');
+    //   contentEl.innerHTML = `
+    //   <div>
+    //     <h1>
+    //       <strong>Multiple Choice Question</strong>
+    //     </h1>
+    //     <br/>
+    //     <button type="button" class="btn btn-primary" style="background: #007bff">Answer 1</button>
+    //     <br/><br/>
+    //     <button type="button" class="btn btn-primary" style="background: #007bff">Answer 2</button>
+    //     <br/><br/>
+    //     <button type="button" class="btn btn-primary" style="background: #007bff">Answer 3</button>
+    //   </div>
+    //   `;
 
-      // Modals are temporary by default. They dispose themselves when they are
-      // closed; so, we can create a new one each time the player is paused and
-      // not worry about leaving extra nodes hanging around.
-      const modal = this.player?.createModal(contentEl, {});
+    //   // Modals are temporary by default. They dispose themselves when they are
+    //   // closed; so, we can create a new one each time the player is paused and
+    //   // not worry about leaving extra nodes hanging around.
+    //   const modal = this.player?.createModal(contentEl, {});
 
-      // When the modal closes, resume playback.
-      modal?.on('modalclose', () => {
-        this.player?.play();
-      });
+    //   // When the modal closes, resume playback.
+    //   modal?.on('modalclose', () => {
+    //     this.player?.play();
+    //   });
+    // });
+
+    var pauseTime = 10;
+    var myPlayer = this.player;
+    myPlayer.on('timeupdate', () => {
+      if (myPlayer.currentTime() >= pauseTime) {
+        myPlayer.pause();
+        pauseTime = Number(myPlayer.currentTime() + 999);
+      }
     });
   }
 
